4ec4734210e92471fca3e4c32fca482619c5b717
Luis Perestrelo <100175545+LuisHPerestrelo@users.noreply.github.com>
2022-05-05 12:16:24 -0300

    Update Ex_29_while.java

--- Ex7_21_ao_31/Ex_29_while.java
+++ Ex7_21_ao_31/Ex_29_while.java
@@ -7,14 +7,14 @@ public class Ex_29_while {
 		
 		int f1 = 1, f2 = 0, i = 0;
 		
-		System.out.printf("\n%d \n%d", f1);
+		System.out.printf("\n%d", f1);
 		while (i<30) {
 		
 		f1 = f1 + f2;
 		f2 = f1 - f2;
 		i++;
 		
-		System.out.printf("\n%d \n%d", f1);
+		System.out.printf("\n%d", f1);
 		}
 	
 			
